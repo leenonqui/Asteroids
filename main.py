acb0149ec0a42fb0ea8ac2e179ecbfe7344f38a9
leenonqui <onjo2001@gmail.com>
2024-10-30 11:00:29 +0100

    add rotatate() functionality to Player

--- main.py
+++ main.py
@@ -12,14 +12,17 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    player1 = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player1.draw(screen)
+        player.draw(screen)
+
+        player.update(dt)
+
         pygame.display.flip()
         dt = clock.tick(FPS) #60fps / 1000
 
