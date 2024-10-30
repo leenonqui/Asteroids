6249acf8ca91fb8b144c9ec63a111e85b2cfe3e3
leenonqui <onjo2001@gmail.com>
2024-10-30 09:03:44 +0100

    add circleshape.py

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -16,6 +18,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(FPS) #60fps / 1000
 
 
 if __name__ == "__main__":
