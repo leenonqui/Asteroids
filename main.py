d8036646fdc524e775a1183bab495c14c6b18d8f
leenonqui <onjo2001@gmail.com>
2024-10-30 09:51:55 +0100

    found BUG

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 
 def main():
@@ -11,6 +12,7 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player1 = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
         for event in pygame.event.get():
@@ -18,7 +20,9 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        player1.draw(screen)
         dt = clock.tick(FPS) #60fps / 1000
+
 
 
 if __name__ == "__main__":
