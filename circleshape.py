d37180d5756956d6ea35b7940b7ef0f1dcb68a99
leenonqui <onjo2001@gmail.com>
2024-11-01 13:15:53 +0100

    add asteroids, asteroid field and collision functionality

--- circleshape.py
+++ circleshape.py
@@ -13,6 +13,15 @@ class CircleShape(pygame.sprite.Sprite):
         self.velocity = pygame.Vector2(0, 0)
         self.radius = radius
 
+    def collision(self, other):
+        # if only I had finished reading the assignment way
+        return self.position.distance_to(other.position) <= self.radius + other.radius
+
+        # my brute force way
+        '''s_x,s_y = self.position.xy
+        o_x, o_y = other.position.xy
+        return ((o_x - s_x)**2 + (o_y - s_y)**2)**.5 <= (self.radius + other.radius)'''
+
     def draw(self, screen):
         # sub-classes must override
         pass
--- main.py
+++ main.py
@@ -2,27 +2,55 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from asteroidfield import AsteroidField
 from constants import *
-from player import *
+from player import Player
+from asteroid import Asteroid
 
 
 def main():
 
+    # setup
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    AsteroidField.containers = updatable
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    asteroid_field = AsteroidField()
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
-
         screen.fill("black")
-        player.draw(screen)
+
+        # update updatable items
+        for item in updatable:
+            item.update(dt)
+
+        # draw drawable items
+        for item in drawable:
+            item.draw(screen)
+
+        # chacks for collisions
+        for asteroid in asteroids:
+            if player.collision(asteroid):
+                pygame.quit()
+                print("Game Over!")
+                return
+
+        # refreshes
         pygame.display.flip()
 
         #limit the framerate to 60 FPS
